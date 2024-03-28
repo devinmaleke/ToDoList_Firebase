42bb7879374cd039669bf9f43d1d60b432b19f9c
Devin <devin@cyberindo-sinergi.com>
2024-03-28 14:07:59 +0700

    Initial Commit

---
+++ Todo App Firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todo App Firebase
+//
+//  Created by Cyberindo Sinergi Sistem on 28/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
